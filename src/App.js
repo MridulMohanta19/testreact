fed433c49cd04e7af611eafd4ed95ec9ccccc14e
MridulMohanta19 <mm8203@srmist.edu.in>
2023-07-10 01:39:47 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import Textarea from './components/Textarea';
-
+import { useState } from 'react';
 function App() {
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+    if(mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+    }
+    else {
+      setMode("light");
+      document.body.style.backgroundColor = "light";
+    }
+  }
   return (
     <>
-  <Navbar tittle= 'TextUtils'/>
+  <Navbar tittle= 'TextUtils' mode={mode} toggleMode={toggleMode}/>
   <div className='container my-3' >
-  {/* <Textarea heading="Enter the Text"/> */}
-  <About/>
+  <Textarea heading="Enter the Text" mode={mode}/>
+  {/* <About/> */}
   </div>
     </>
   );
